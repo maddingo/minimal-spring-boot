176a00735cda458f0ee6e8d23f25cdefe4320ec0
Martin Goldhahn <mgoldhahn@gmail.com>
2022-05-11 19:59:24 +0200

    Logging RestTemplate

--- src/main/java/no/maddin/minimal/MinimalApplication.java
+++ src/main/java/no/maddin/minimal/MinimalApplication.java
@@ -1,12 +1,22 @@
 package no.maddin.minimal;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Slf4j
 public class MinimalApplication {
     public static void main(String... args) {
         SpringApplication.run(MinimalApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        log.info("Creating RestTemplate Bean");
+        return builder.build();
     }
 }
--- src/test/java/no/maddin/minimal/MinimalControllerTest.java
+++ src/test/java/no/maddin/minimal/MinimalControllerTest.java
@@ -1,10 +1,17 @@
 package no.maddin.minimal;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.AbstractRefreshableApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -12,6 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
 public class MinimalControllerTest {
 
     @LocalServerPort
@@ -22,11 +30,25 @@ public class MinimalControllerTest {
 
     @Test
     void timeResponse() {
-        ResponseEntity<TimeResponse> timeResponse = restTemplate.getForEntity("http://localhost:" + serverPort + "/now/utc", TimeResponse.class);
+        ResponseEntity<TimeResponse> response = restTemplate.getForEntity("http://localhost:" + serverPort + "/now/utc", TimeResponse.class);
 
-        assertThat(timeResponse, allOf(
+        assertThat(response, allOf(
             hasProperty("statusCode", equalTo(HttpStatus.OK)),
             hasProperty("body", hasProperty("timeString", instanceOf(String.class)))
+        ));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"London", "Berlin"})
+    void weatherResponse(String city) {
+
+        log.info("TEST before weather");
+        ResponseEntity<WeatherResponse> response = restTemplate.getForEntity("http://localhost:" + serverPort + "/weather/" + city, WeatherResponse.class);
+        log.info("TEST after weather");
+
+        assertThat(response, allOf(
+            hasProperty("statusCode", equalTo(HttpStatus.OK)),
+            hasProperty("body", hasProperty("temperature", containsString("°C")))
         ));
     }
 }
